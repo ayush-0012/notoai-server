ffa7d7dd540a65a265966702fad020e350a2daae
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-31 11:33:42 +0530

    fixed env config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,20 @@
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+
+dotenv.config({
+  path: envFile,
+});
+
+console.log("Cloudinary Config:", {
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
